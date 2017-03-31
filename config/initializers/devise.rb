835ea291ec17ab79c5d903e7033d24ca9ad573b0
Herman Falchi <herman.falchi@alumnos.usm.cl>
2017-03-31 01:42:02 -0300

    test commit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,6 +5,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
+  config.secret_key = 'c5ea4dd87370c2c7074172dfe7dc4f9365cb260bdc2cd98a0171cd7a4f60b4da78c71ce4dda61b9b4c3e5ebd5cb39d0bfdc73b9ee4da4e5e92d5d8e78e506735'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 201308301400) do
+ActiveRecord::Schema.define(:version => 20140106085942) do
 
   create_table "arcos", :force => true do |t|
     t.boolean "bidi",     :null => false
@@ -66,6 +66,24 @@ ActiveRecord::Schema.define(:version => 201308301400) do
   end
 
   add_index "proyectos", ["nombre"], :name => "index_proyectos_on_nombre"
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   create_table "usuarios", :force => true do |t|
     t.string   "usuario"
